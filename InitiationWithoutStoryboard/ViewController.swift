ed775dcf52fee12e3f794a1ea2c4bc2c25117737
LinusLing <linusling419@gmail.com>
2020-08-03 14:38:20 +0800

    init

---
+++ InitiationWithoutStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InitiationWithoutStoryboard
+//
+//  Created by linus on 2020/8/3.
+//  Copyright © 2020 nioers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
